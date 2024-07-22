6b84764e503c8c0fc488ab1c73afe12a4a681224
Darkknight-86 <bewick.j@northeastern.edu>
2024-07-22 16:53:22 -0400

    update to match Kangning

--- src/main/java/model/DataMgmt/Stock.java
+++ src/main/java/model/DataMgmt/Stock.java
@@ -1,4 +1,4 @@
-package model;
+package model.DataMgmt;
 
 import com.crazzyghost.alphavantage.timeseries.response.TimeSeriesResponse;
 import com.crazzyghost.alphavantage.timeseries.response.StockUnit;
