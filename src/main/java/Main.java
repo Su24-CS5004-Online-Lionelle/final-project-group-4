fd4c802c5efe3a4efbba3fc005afc0bf44939370
Darkknight-86 <bewick.j@northeastern.edu>
2024-07-23 11:09:39 -0400

    updates to proposal

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,8 @@ import controller.Controller;
 import java.io.IOException;
 
 /**
- * The Main class serves as the entry point for the application.
- * It initializes the Controller with the API key and starts the application
- * logic.
+ * The Main class serves as the entry point for the application. It initializes the Controller with
+ * the API key and starts the application logic.
  */
 public class Main {
     /**
